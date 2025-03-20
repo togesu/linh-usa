e5c9af00bfcff27738f4f9b7fe6869f619fd4e27
togesu <sdqlong@gmail.com>
2025-03-20 16:33:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
     titleWeb: "Quá vip",
     introTitle: 'Hi Lynh usa',
-    introDesc: `Xuân sang tết đến đong đầy
-    Nhưng mà vẫn thiếu chuỗi ngày có em
-    Thơ ca Linh cũng đã xem
-    Bao nhiêu thương nhớ đã đem gửi nàng`,
+    introDesc: `Long làm cái này để rủ Linh đi chơi`,
     btnIntro: 'Nghe chút nhạc nhé!',
     title: 'Chiều mai đi xem thả diều cùng Long nha',
     desc: 'Nếu không muốn đi cũng không sao đâu :v',
     btnYes: 'Linh thích lắm <3',
     btnNo: 'Không đi nha :3',
-    question: 'Quá vip, mấy giờ đi Linh?',
+    question: 'Mấy giờ đi Linh nhỉ?',
     btnReply: 'Gửi thời gian cho Long',
     reply: 'Linh rất thích đi t̶h̶ả̶ ̶d̶i̶ề̶u̶  cùng Long',
-    mess: 'Oki Quá vip',
-    messDesc: 'Chiều mai 16h30, Long vào đón Linh nhé!',
+    mess: 'KK Quá vip',
+    messDesc: 'Mấy giờ đi thì nhắn Long biết nha',
     btnAccept: 'Oki lun <3',
     messLink: 'https://www.facebook.com/messages/t/100035046545890'
 }
